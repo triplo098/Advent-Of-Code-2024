6a1ea6152e3b5364a338c0935f44399b7f598e38
triplo098 <piotr.koronczok@gmail.com>
2024-12-02 08:48:55 +0100

    Solved day 2

--- day_2/1.py
+++ day_2/1.py
@@ -1,11 +1,46 @@
 
-readInput = True #deciding between demo or input
+def is_safe(report):
+    is_dec = True
+    is_asc = True
 
-if readInput == True:
-    fileNameToRead = "input.txt"
-else:
-    fileNameToRead = "demo_input.txt"
+    for i in range(0, len(report) - 1):
 
-data = open(fileNameToRead, "r").read()
+        if report[i] < report[i + 1]:
+            is_dec = False
+        elif  report[i] > report[i + 1]:
+            is_asc = False
 
-print(data)
+        if (not is_dec )and (not is_asc):
+            return False
+
+        diff = abs(report[i] - report[i + 1])
+
+        if diff > 3 or diff < 1:
+            return False
+
+    return True
+
+def main():
+
+    readInput = True #deciding between demo or input
+
+    if readInput == True:
+        fileNameToRead = "input.txt"
+    else:
+        fileNameToRead = "demo_input.txt"
+
+    data = open(fileNameToRead, "r").read()
+
+    data = list(filter(None, data.split("\n")))
+
+    reports = [[int(i) for i in line.split(" ")] for line in data]
+
+    sum = 0
+    for report in reports:
+        if is_safe(report):
+            sum += 1
+
+    print(sum)
+
+if __name__ == "__main__":
+    main()
--- day_2/2.py
+++ day_2/2.py
@@ -1,11 +1,63 @@
+def is_dec(r):
+    for i in range(0, len(r) - 1):
+        if r[i] < r[i + 1]:
+            return False
+    return True
 
-readInput = True #deciding between demo or input
+def is_asc(r):
+    for i in range(0, len(r) - 1):
+        if r[i] > r[i + 1]:
+            return False
+    return True
 
-if readInput == True:
-    fileNameToRead = "input.txt"
-else:
-    fileNameToRead = "demo_input.txt"
+def diff_valid(r):
+    for i in range(0, len(r) - 1):
+        diff = abs(r[i] - r[i + 1])
+        if diff < 1 or diff > 3:
+            return False
+    return True
 
-data = open(fileNameToRead, "r").read()
+def is_safe(report):
 
-print(data)
+    dec = True
+    asc = True
+
+    for i in range(0, len(report)):
+
+        report_removed = report.copy()
+        report_removed.pop(i)
+
+        dec = is_dec(report) or is_dec(report_removed)
+        asc = is_asc(report) or is_asc(report_removed)
+        diff = diff_valid(report) or diff_valid(report_removed)
+
+        # print("d", dec, "a", asc, "diff", diff)
+
+        if (dec or asc) and diff:
+            return True
+
+    return False
+
+def main():
+
+    readInput = True #deciding between demo or input
+
+    if readInput == True:
+        fileNameToRead = "input.txt"
+    else:
+        fileNameToRead = "demo_input.txt"
+
+    data = open(fileNameToRead, "r").read()
+    data = list(filter(None, data.split("\n")))
+
+    reports = [[int(i) for i in line.split(" ")] for line in data]
+
+    sum = 0
+    for report in reports:
+        if is_safe(report):
+            sum += 1
+
+    print(sum)
+
+if __name__ == "__main__":
+    main()
