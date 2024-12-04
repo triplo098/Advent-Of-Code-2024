a54dd7d4bfbf805268b427839b5051c891983585
triplo098 <piotr.koronczok@gmail.com>
2024-12-04 17:29:28 +0100

    Solved part 1 of day 4

--- day_4/1.py
+++ day_4/1.py
@@ -1,3 +1,4 @@
+from itertools import product
 
 def main():
 
@@ -9,8 +10,58 @@ def main():
         fileNameToRead = "demo_input.txt"
 
     data = open(fileNameToRead, "r").read()
-
     print(data)
+
+    matrix = [[c for c in line] for line in list(filter(None, data.split("\n") ))]
+
+    print(matrix)
+    sum = 0
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+
+            c = matrix[i][j]
+
+            if c == 'X':
+
+                base_directions = [-1, 0, 1] #    x, y
+                directions = product(base_directions, repeat=2)
+
+                for dir in directions:
+                    if dir[0] == 0 and dir[1] == 0:
+                        continue
+
+                    new_i = i + dir[0]
+                    new_j = j + dir[1]
+
+                    print("dir: ", dir)
+                    mas = ['M', 'A', 'S']
+                    check = []
+                    for l in mas:
+
+                        # print("i ", i, "j ", j)
+                        # print("new_i: ", new_i, "new_j: ", new_j)
+
+                        if new_i < 0 or new_j < 0 or new_i >= len(matrix) or new_j >= len(matrix[0]):
+                            break
+
+                        selected_char = matrix[new_i][new_j]
+
+                        # print("sc: ", selected_char)
+                        # print("l: ", l)
+                        if selected_char != l:
+                            break
+
+                        check.append(selected_char)
+                        new_i += dir[0]
+                        new_j += dir[1]
+
+                    print("check: ", check)
+                    if check == mas:
+                        sum += 1
+
+    print(sum)
+
 
 
 if __name__ == "__main__":
--- day_4/2.py
+++ day_4/2.py
@@ -1,3 +1,6 @@
+from itertools import product
+import numpy as np
+
 
 def main():
 
@@ -9,8 +12,44 @@ def main():
         fileNameToRead = "demo_input.txt"
 
     data = open(fileNameToRead, "r").read()
-
     print(data)
+
+    matrix = [[c for c in line] for line in list(filter(None, data.split("\n") ))]
+
+    a = np.array(matrix)
+
+    print(a.shape)
+
+
+    print(matrix)
+    sum = 0
+
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+
+            c = matrix[i][j]
+
+            if c == 'A':
+
+                # print("i ", i, "j ", j)
+                if i < 1 or j < 1 or i >= len(matrix) - 1 or j >= len(matrix[0]) - 1:
+                    break
+
+                diag_1 = {matrix[i - 1][j - 1], matrix[i + 1][j + 1]}
+                diag_2 = {matrix[i - 1][j + 1], matrix[i + 1][j - 1]}
+
+                # print(diag_1)
+                # print(diag_2)
+                ms = {'M', 'S'}
+
+                if ms == diag_1 and ms == diag_2:
+                    sum += 1
+                    # print("MAS")
+
+
+    print(sum)
+
 
 
 if __name__ == "__main__":
