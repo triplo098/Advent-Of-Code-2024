cd310eb3e703794a08cdc6f4fcb0c1017472dd05
triplo098 <piotr.koronczok@gmail.com>
2024-12-05 07:47:14 +0100

    Solved day 5

--- day_5/1.py
+++ day_5/1.py
@@ -1,4 +1,3 @@
-
 def main():
 
     readInput = True #deciding between demo or input
@@ -10,8 +9,44 @@ def main():
 
     data = open(fileNameToRead, "r").read()
 
-    print(data)
+    data = data.split("\n\n")
 
+    rules = [list(map(int, i.split("|"))) for i in data[0].split("\n")]
+
+    updates = [list(map(int, i.split(","))) for i in filter(None, data[1].split("\n"))]
+
+    sum = 0
+
+    for update in updates:
+        visited = update[:]
+
+        valid = True
+
+        print("visited:", visited)
+
+        for page in visited:
+
+            applied_rules = [rule for rule in rules if rule[0] == page]
+
+            print("page", page)
+            print("ar", applied_rules)
+
+            for rule in applied_rules:
+
+                print(rule)
+
+                if rule[1] in visited and visited.index(page) > visited.index(rule[1]):
+
+                    print("NOT_VALID")
+
+                    valid = False
+
+        if valid:
+            print("VALID")
+            sum += update[(len(update) // 2)]
+            print("sum += ", sum)
+
+    print(sum)
 
 if __name__ == "__main__":
     main()
--- day_5/2.py
+++ day_5/2.py
@@ -1,3 +1,18 @@
+rules = []
+
+def is_valid(visited):
+
+    global rules
+    for page in visited:
+
+        applied_rules = [rule for rule in rules if rule[0] == page]
+
+        for rule in applied_rules:
+
+            if rule[1] in visited and visited.index(page) > visited.index(rule[1]):
+                return False
+
+    return True
 
 def main():
 
@@ -8,10 +23,44 @@ def main():
     else:
         fileNameToRead = "demo_input.txt"
 
-    data = open(fileNameToRead, "r").read()
+    data = open(fileNameToRead, "r").read().split("\n\n")
 
-    print(data)
+    global rules
 
+    rules = [list(map(int, i.split("|"))) for i in data[0].split("\n")]
+    updates = [list(map(int, i.split(","))) for i in filter(None, data[1].split("\n"))]
+
+    sum = 0
+
+    for update in updates:
+        visited = update[:]
+
+        valid = is_valid(visited)
+
+        if not valid:
+
+            while (not is_valid(visited)):
+
+                for page in visited:
+
+                    applied_rules = [rule for rule in rules if rule[0] == page]
+
+                    for rule in applied_rules:
+
+                        i_page = visited.index(page)
+
+                        if rule[1] not in visited:
+                            continue
+
+                        i_sec = visited.index(rule[1])
+
+                        if i_page > i_sec:
+                            # print("swapped")
+                            visited[i_page], visited[i_sec] = visited[i_sec], visited[i_page]
+
+            sum += visited[(len(visited) // 2)]
+
+    print(sum)
 
 if __name__ == "__main__":
     main()
