97c92612235ef26c6166c27ee52ba9a5422b418d
triplo098 <piotr.koronczok@gmail.com>
2024-12-06 11:30:28 +0100

    Solved day 6

--- day_6/1.py
+++ day_6/1.py
@@ -1,3 +1,13 @@
+dirs = [[0, -1], [-1, 0], [0, 1], [1, 0]]
+dirs_keys = ['<', '^', '>', 'v']
+
+def get_direction(orientation_char):
+    d = dirs[dirs_keys.index(orientation_char)]
+    return d
+
+def turn_90_right(d):
+    d_out = dirs[(dirs.index(d) + 1) % len(dirs)]
+    return d_out
 
 def main():
 
@@ -12,6 +22,33 @@ def main():
 
     print(data)
 
+    grid = [list(line) for line in filter(None,data.split("\n"))]
+
+    pos = []
+
+    for row in grid:
+        for el in row:
+            if el != '.' and el != '#' and el != 'X':
+               pos = [grid.index(row), row.index(el)]
+
+    d = get_direction(grid[pos[0]][pos[1]])
+    grid[pos[0]][pos[1]] = 'X'
+
+    while pos[0] + d[0] >= 0 and pos[0] + d[0] < len(grid) and pos[1] + d[1] >= 0 and pos[1] + d[1] < len(grid[0]):
+
+        if grid[pos[0] + d[0]][pos[1] + d[1]] == '#':
+           d = turn_90_right(d)
+           continue
+
+        pos = [pos[0] + d[0], pos[1] + d[1]]
+        grid[pos[0]][pos[1]] = 'X'
+
+    my_sum = sum([row.count('X') for row in grid])
+
+    print(my_sum)
+
+    # for row in grid:
+    #     print(str(row))
 
 if __name__ == "__main__":
     main()
--- day_6/2.py
+++ day_6/2.py
@@ -1,3 +1,49 @@
+dirs = [[0, -1], [-1, 0], [0, 1], [1, 0]]
+dirs_keys = ['<', '^', '>', 'v']
+
+#  1747 too high
+def get_direction(orientation_char):
+    d = dirs[dirs_keys.index(orientation_char)]
+    return d
+
+def turn_90_right(d):
+    d_out = dirs[(dirs.index(d) + 1) % len(dirs)]
+    return d_out
+
+def perform_walking(grid, max_steps):
+
+    pos = []
+    for row in grid:
+        for el in row:
+            if el != '.' and el != '#' and el != 'X' and el != 'O':
+               pos = [grid.index(row), row.index(el)]
+
+    d = get_direction(grid[pos[0]][pos[1]])
+    grid[pos[0]][pos[1]] = 'X'
+    x_pos = []
+    steps = 0
+
+    while pos[0] + d[0] >= 0 and pos[0] + d[0] < len(grid) and pos[1] + d[1] >= 0 and pos[1] + d[1] < len(grid[0]):
+        c = grid[pos[0] + d[0]][pos[1] + d[1]]
+        if c == '#' or c == 'O':
+           d = turn_90_right(d)
+           continue
+
+        pos = [pos[0] + d[0], pos[1] + d[1]]
+        steps += 1
+        if grid[pos[0]][pos[1]] != 'X':
+            grid[pos[0]][pos[1]] = 'X'
+            x_pos.append([pos[0], pos[1]])
+
+        if steps > max_steps:
+            for row in grid:
+                print(str(row))
+
+            print(" --- ")
+            break
+
+    return steps, x_pos
+
 
 def main():
 
@@ -10,8 +56,25 @@ def main():
 
     data = open(fileNameToRead, "r").read()
 
-    print(data)
 
+    grid = [list(line) for line in filter(None,data.split("\n"))]
+    base_grid = [row[:] for row in grid]
+
+    MAX = pow(len(base_grid), 2)
+    my_sum = 0
+
+    max_steps, x_pos = perform_walking(grid, MAX)
+    print(max_steps)
+    for x_p in x_pos:
+
+        temp_grid = [row[:] for row in base_grid]
+        temp_grid[x_p[0]][x_p[1]] = 'O'
+        steps = perform_walking(temp_grid, MAX)[0]
+
+        if steps > MAX:
+            my_sum += 1
+
+    print(my_sum)
 
 if __name__ == "__main__":
     main()
