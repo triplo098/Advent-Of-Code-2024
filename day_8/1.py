1d5b2de30b51ef716189214fb85dd3b0d011f5c3
triplo098 <piotr.koronczok@gmail.com>
2024-12-08 09:54:52 +0100

    Solved day 8

--- day_8/1.py
+++ day_8/1.py
@@ -1,3 +1,14 @@
+from itertools import combinations
+
+def is_in_boundry(position, grid):
+
+    i = position[0]
+    j = position[1]
+
+    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+        return False
+
+    return True
 
 def main():
 
@@ -10,8 +21,48 @@ def main():
 
     data = open(fileNameToRead, "r").read()
 
-    print(data)
+    grid = [list(line) for line in filter(None, data.split("\n"))]
 
+    for row in grid:
+        print(str(row))
+
+    frequencies = dict()
+    for i, row in enumerate(grid):
+        for j, c in enumerate(row):
+
+            if c == '.':
+                continue
+
+            if c not in frequencies.keys():
+                frequencies[c] = []
+
+            frequencies[c].append([i, j])
+
+    for pos in frequencies.values():
+        permuts = combinations(pos, 2)
+
+        for p in permuts:
+
+            print(p)
+
+            d_i = p[0][0] - p[1][0]
+            d_j = p[0][1] - p[1][1]
+
+            print(d_i, d_j)
+
+            coords_1 = [p[0][0] + d_i, p[0][1] + d_j]
+            coords_2 = [p[1][0] - d_i, p[1][1] - d_j]
+
+            if is_in_boundry(coords_1, grid):
+                grid[coords_1[0]][coords_1[1]] = '#'
+
+            if is_in_boundry(coords_2, grid):
+                grid[coords_2[0]][coords_2[1]] = '#'
+
+    for row in grid:
+        print(str(row))
+
+    print(sum([row.count('#') for row in grid]))
 
 if __name__ == "__main__":
     main()
--- day_8/2.py
+++ day_8/2.py
@@ -1,3 +1,14 @@
+from itertools import combinations
+
+def is_in_boundry(position, grid):
+
+    i = position[0]
+    j = position[1]
+
+    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+        return False
+
+    return True
 
 def main():
 
@@ -10,7 +21,49 @@ def main():
 
     data = open(fileNameToRead, "r").read()
 
-    print(data)
+    grid = [list(line) for line in filter(None, data.split("\n"))]
+
+    for row in grid:
+        print(str(row))
+
+    frequencies = dict()
+    for i, row in enumerate(grid):
+        for j, c in enumerate(row):
+            if c == '.':
+                continue
+
+            if c not in frequencies.keys():
+                frequencies[c] = []
+
+            frequencies[c].append([i, j])
+
+    for pos in frequencies.values():
+
+        permuts = combinations(pos, 2)
+
+        for p in permuts:
+
+            d_i = p[0][0] - p[1][0]
+            d_j = p[0][1] - p[1][1]
+
+            coords = [p[0][0] + d_i, p[0][1] + d_j]
+
+            while is_in_boundry(coords, grid):
+                grid[coords[0]][coords[1]] = '#'
+                coords[0] += d_i
+                coords[1] += d_j
+
+            coords = p[0][:]
+
+            while is_in_boundry(coords, grid):
+                grid[coords[0]][coords[1]] = '#'
+                coords[0] -= d_i
+                coords[1] -= d_j
+
+    for row in grid:
+        print(str(row))
+
+    print(sum([row.count('#') for row in grid]))
 
 
 if __name__ == "__main__":
