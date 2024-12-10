8034be5abe44c8826d6bd209bf818b2e9ffdc5d7
triplo098 <piotr.koronczok@gmail.com>
2024-12-10 13:15:12 +0100

    Solved day 10

--- day_10/2.py
+++ day_10/2.py
@@ -1,6 +1,48 @@
+import queue
+grid = []
+
+def bsf_search(starting_position):
+    global grid
+    q = queue.Queue()
+    count = 0
+    q.put(starting_position)
+
+    while not q.empty():
+        pos = q.get()
+
+        if grid[pos[0]][pos[1]] == 9:
+            count += 1
+            continue
+
+        positions = get_adjecent_positions(pos)
+
+        for p in positions:
+            q.put(p)
+
+    return count
+
+def get_adjecent_positions(pos):
+    global grid
+
+    out_pos = []
+    directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+    v = grid[pos[0]][pos[1]]
+
+    for p in directions:
+
+        i = pos[0] + p[0]
+        j = pos[1] + p[1]
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+            continue
+
+        v_new = grid[i][j]
+        if  v_new - v == 1:
+            out_pos.append([i, j])
+
+    return out_pos
 
 def main():
-
+    global grid
     readInput = True #deciding between demo or input
 
     if readInput == True:
@@ -10,8 +52,15 @@ def main():
 
     data = open(fileNameToRead, "r").read()
 
-    print(data)
+    grid = [[int(x) for x in row] for row in filter(None, data.split("\n"))]
 
+    out = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 0:
+                out += bsf_search([i, j])
+
+    print(out)
 
 if __name__ == "__main__":
     main()
